615a65d26d524f71c7729b031cca4b27d584ce03
Felyp-Henrique <felyphenrique2015@gmail.com>
2022-10-26 07:05:10 -0300

    dev: escrevendo insercao e remocao de nos na lista encadeada

--- pkg/list/linked.go
+++ pkg/list/linked.go
@@ -37,19 +37,59 @@ func (l LinkedList[T]) Last() *LinkedNode[T] {
 }
 
 func (l LinkedList[T]) Get(index int) *LinkedNode[T] {
-	return nil
+	var (
+		node    *LinkedNode[T] = l.first
+		current int            = 0
+	)
+	if l.length == 0 || index >= l.length || index < 0 {
+		return nil
+	}
+	if index == 0 {
+		return l.first
+	}
+	if index == l.length-1 {
+		return l.last
+	}
+	for current != index {
+		node = node.next
+		current += 1
+	}
+	return node
 }
 
 func (l *LinkedList[T]) Add(value T) {
 	var (
-		node *LinkedNode[T] = &LinkedNode[T]{}
+		node *LinkedNode[T] = &LinkedNode[T]{
+			value: value,
+		}
 	)
-	node.value = value
+	if l.first == nil {
+		l.first = node
+	}
+	if l.last == nil {
+		l.last = node
+		node.previous = l.last
+	} else {
+		l.last.next = node
+		node.previous = l.last
+		l.last = node
+	}
 	l.length += 1
 }
 
 func (l *LinkedList[T]) Remove(index int) {
-	l.length -= 1
+	var (
+		node *LinkedNode[T] = nil
+	)
+	if node = l.Get(index); node != nil {
+		if node == l.last {
+			//l.last.previous
+		}
+		node.next = node.previous
+		node.previous = node.next
+		node = nil
+		l.length -= 1
+	}
 }
 
 func (l *LinkedList[T]) Push(index int, value T) {
--- pkg/list/linked_test.go
+++ pkg/list/linked_test.go
@@ -1,7 +1,27 @@
 package list_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Felyp-Henrique/algorithm/pkg/list"
+)
 
 func TestLinkedList(t *testing.T) {
-
+	var (
+		list list.LinkedList[int] = list.LinkedList[int]{}
+	)
+	list.Add(20)
+	list.Add(80)
+	list.Add(50)
+	list.Add(70)
+	fmt.Println(list.Get(1).Data())
+	fmt.Println(list.Get(2).Data())
+	fmt.Println(list.Get(3).Data())
+	fmt.Println(list.Length())
+	list.Remove(4)
+	fmt.Println(list.Get(1).Data())
+	fmt.Println(list.Get(2).Data())
+	fmt.Println(list.Get(3))
+	fmt.Println(list.Length())
 }
